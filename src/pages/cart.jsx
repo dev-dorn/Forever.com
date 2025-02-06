5aeadff1e191b521a303975a7e138b5784c712e2
root <berrymundia@gmail.com>
2025-02-06 19:58:35 +0000

    added the cart page and it is updating as expected

--- src/pages/cart.jsx
+++ src/pages/cart.jsx
@@ -1,10 +1,48 @@
+import React, { useEffect, useState, useContext } from 'react';
+import { ShopContext } from '../context/ShopContext';
+import Title from '../components/Title';
 
 const Cart = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const { products, currency, cartItems } = useContext(ShopContext);
 
-export default Cart
+  const [cartData, setCartData] = useState([]);
+
+  useEffect(() => {
+    const tempData = [];
+    for (const item in cartItems) {
+      for (const size in cartItems[item]) {
+        if (cartItems[item][size] > 0) {
+          tempData.push({
+            _id: item,
+            size: size,
+            quantity: cartItems[item][size],
+          });
+        }
+      }
+    }
+    setCartData(tempData);
+  }, [cartItems]);
+
+  return (
+    <div className='border-t pt-14'>
+      <div className='text 2xl mb-3'>
+        <Title text1={'YOUR'} text2={'CART'} />
+      </div>
+      <div className=''>
+        {cartData.map((item, index) => {
+          const productData = products.find((product) => product._id === item._id);
+          return (
+            <div key={index} className='py-4 border-t border-b text-gray-700 grid grid-cols-[4fr_0.5fr_0.5fr] sm:grid-cols-[4fr_2fr_0.5fr] items-center gap-4 '>
+              <div className='flex items-start gap-6'>
+                <img className='w-16 sm:w-20' src={productData.image[0]} alt='' />
+                <p className='text-xs sm:text-lg font-medium'>{productData.name}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
