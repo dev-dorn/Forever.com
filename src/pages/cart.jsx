ffc667214748fc1db212a83824b07408b7883702
root <berrymundia@gmail.com>
2025-02-06 20:17:07 +0000

    i added a functionality that shows how many items you have selected in your cart and the size

--- src/pages/cart.jsx
+++ src/pages/cart.jsx
@@ -35,8 +35,16 @@ const Cart = () => {
             <div key={index} className='py-4 border-t border-b text-gray-700 grid grid-cols-[4fr_0.5fr_0.5fr] sm:grid-cols-[4fr_2fr_0.5fr] items-center gap-4 '>
               <div className='flex items-start gap-6'>
                 <img className='w-16 sm:w-20' src={productData.image[0]} alt='' />
+                <div>
                 <p className='text-xs sm:text-lg font-medium'>{productData.name}</p>
+              <div className='flex items-center gap-5 mt-2'>
+                <p>{currency}{productData.price}</p>
+                <p className='px-2 sm:px-3 sm:py-1 border bg-slate-50'>{item.size}</p>
+
               </div>
+              </div>
+              </div>
+              <input className='border max-w-10 sm:max-w-20 px-1 sm:px-2 px-1' type='number' min={1} defaultValue={item.quantity}/>
             </div>
           );
         })}
